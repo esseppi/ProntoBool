d88cc0ac79800d8263d5d006ebc3fb5fb24059b1
Your Name <you@example.com>
2022-06-11 09:31:30 +0200

    profile controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Professionist\ProfileController;
+use App\Http\Controllers\Professionist\Profile\ProfileController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +13,14 @@ use App\Http\Controllers\Professionist\ProfileController;
 | contains the "web" middleware group. Now create something great!
 |
 */
+// REGISTERED USER SECTION
+Route::middleware('auth')
+    ->name('professionist.')
+    ->prefix('professionist')
+    ->group(function () {
+        Route::get('myProfile/edit', 'App\Http\Controllers\Professionist\Profile\ProfileController@edit');
+        Route::resource('myProfile', ProfileController::class);
+    });
 
 
 Route::get('/', function () {
@@ -32,18 +40,3 @@ require __DIR__ . '/auth.php';
 Route::get("{any?}", function () {
     return view("spa");
 })->where("any", ".*");
-// REGISTERED USER SECTION
-// Route::middleware('auth')
-//     ->name('professionist.')
-//     ->prefix('professionist')
-//     ->group(function () {
-//         Route::resource('myProfile', ProfileController::class);
-//         // Route::resource('/myProfile', 'ProfileController');
-//         // Route::post('/slugger', 'HomeController@slugger')->name('slugger');
-//         // Route::get('/posts/my-posts', 'PostController@myindex')->name('posts.myindex');
-//         // Route::resource('/posts', 'PostController');
-//         // Route::resource('/categories', 'CategoryController');
-//         // Route::get('/account', 'UserController@edit')->name('account.edit');
-//         // Route::post('/account', 'UserController@update')->name('account.update');
-//         // Route::delete('/account', 'UserController@destroy')->name('account.destroy');
-//     });
