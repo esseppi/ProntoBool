10d2d5bf7fd16fd49fb96c9dd21a9c56de9aa801
cristianllambro <llambro.cristian1996@gmail.comm>
2022-06-13 11:35:51 +0200

    message

--- database/migrations/2022_06_13_090611_create_leads_table.php
+++ database/migrations/2022_06_13_090611_create_leads_table.php
@@ -15,6 +15,9 @@ class CreateLeadsTable extends Migration
     {
         Schema::create('leads', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->string('message');
             $table->timestamps();
         });
     }
--- database/seeders/LeadSeeder.php
+++ database/seeders/LeadSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Professionist\Lead;
+use Faker\Generator as Faker;
 
 class LeadSeeder extends Seeder
 {
@@ -13,6 +15,12 @@ class LeadSeeder extends Seeder
      */
     public function run()
     {
-        //
+        for ($i = 0; $i < 5; $i++) {
+            Lead::create([
+                'name' => $faker->name,
+                'email' => $faker->email,
+                'message' => $faker->text,
+            ]);
+        }
     }
 }
